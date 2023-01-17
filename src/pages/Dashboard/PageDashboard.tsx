6a0bab2dc9e4fa2bbe2f86c661b81026d2be1d0d
Oscar Yang <oscaryang1220@gmail.com>
2023-01-18 04:36:40 +0800

    feat: add sidebar styles

--- src/pages/Dashboard/PageDashboard.tsx
+++ src/pages/Dashboard/PageDashboard.tsx
@@ -1,19 +1,8 @@
 import React from "react";
-// import IconButton from "../../components/baseCPN/iconButton";
 import "./PageDashboard.scss";
+
 function PageDashboard() {
-  return (
-    <div className="dashboard-content">
-      <div className="dashboard-nav">
-        <div className="dashboard-title">Good morning UserName</div>
-        <div className="dashboard-nav-button-group button-group">
-          <div>
-            <div className="dashboard-addContracts"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="page-content-wrapper"></div>;
 }
 
 export default PageDashboard;
